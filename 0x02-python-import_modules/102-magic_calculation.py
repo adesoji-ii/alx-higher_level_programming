be0d9050b9285e8d09ac9b277a3f95882304dcbb
adesoji-ii <festusoluwapelumi@gmail.com>
2023-11-30 13:43:34 -0800

    advanced tasks

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    """Match bytecode provided by Holberton School."""
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        return add(a, b) + sum(range(4, 6))
+    else:
+        return sub(a, b)
