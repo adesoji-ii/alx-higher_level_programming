126ad3e33c4b13e4c655edf4981b32276140d9fe
adesoji-ii <festusoluwapelumi@gmail.com>
2023-11-30 13:34:44 -0800

    mandatory tasks update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,9 +5,6 @@ if __name__ == "__main__":
     import sys
 
     total = 0
-    for i in range(1, len(sys.argv)):  # Start from index 1 to skip the script name
-        try:
-            total += int(sys.argv[i])
-        except ValueError:
-            print(f"Invalid input: {sys.argv[i]}. Skipping...")
-    print("Sum:", total)
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -2,8 +2,6 @@
 
 if __name__ == "__main__":
     """Print the value of variable a from variable_load_5."""
-    try:
-        from variable_load_5 import a
-        print(a)
-    except ImportError:
-        print("Variable 'a' not found in variable_load_5 module.")
+    from variable_load_5 import a
+
+    print(a)
